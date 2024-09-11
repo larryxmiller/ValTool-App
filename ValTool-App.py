b64dcec8c932339856525b05d4bad1e2d6178048
Larry Miller <larryxmiller@gmail.com>
2024-09-11 14:33:39 +0800

    Update ValTool-App.py

--- ValTool-App.py
+++ ValTool-App.py
@@ -213,5 +213,5 @@ if st.button('Submit'):
     for bar in bars:
         width = bar.get_width()
         ax.text(width, bar.get_y() + bar.get_height() / 2, f'{width:.2f}%',
-                va='center', ha='left', color='black')
+                va='center', ha='left', color='black')
     st.pyplot(fig)
